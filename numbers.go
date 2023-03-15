f4bc1ddcdfc44f09f8458e760e6b0699bb87b062
Lyosha0i <96415100+Lyosha0i@users.noreply.github.com>
2023-03-15 19:17:39 +0400

    Added numbers.go

--- numbers.go
+++ numbers.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-
-	//"flag"
-
-	//"bufio"
+	"flag"
 	"fmt"
-
-	//"io/ioutil"
-	//"log"
-	//"net/http"
 	"math/rand"
 	"os"
 	"sync"
@@ -18,110 +11,50 @@ import (
 
 // wg — глобальный sync.WaitGroup
 var wg sync.WaitGroup
-
-func generator(ch chan int) {
+//generator — генерация случайного числа в случае недостаточного количества чисел
+func generator(ch chan int, arr []int, length int) {
 	defer wg.Done()
-	//arr := make([]int, 0, 100)
-	//fmt.Println(arr)
+	rand.Seed(time.Now().UnixNano())
 	var x int
-	//var i int
 	var min = 1
 	var max = 101
 	x = min + rand.Intn(max-min)
-	ch <- x
-	//close(ch)
-	//close(chA)
+	if len(arr) < length {
+		ch <- x
+	}
 }
-func isUnique(ch chan int, arr []int) []int {
-	// defer wg.Done()
-	var temp int
-	for a := range ch {
-		temp = a
-		//println(temp)
+//isUnique — проверка сгенерированного числа на уникальность
+func isUnique(ch chan int, arr []int, length int) {
+	defer wg.Done()
+	for len(arr) < length{
+		x := <-ch
 		var i int
 		for i = 0; i < len(arr); i++ {
-			if temp != arr[i] {
-				break
+			if x == arr[i] {
+				break//если число неуникально
 			}
-			//bufio.NewReader(os.Stdin).ReadBytes('\n')
 		}
-		if i == len(arr) {
-			arr = append(arr, temp)
-			//bufio.NewReader(os.Stdin).ReadBytes('\n')
-			fmt.Println(arr)
+		if i == len(arr) {//если уникально
+			arr = append(arr, x)
+			fmt.Print(fmt.Sprintf("%d ",x))
+		} else {
+			wg.Add(1)
+			go generator(ch, arr, length)
 		}
 	}
-	return arr
 }
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	arr := make([]int, 0, 100)
-	intCh := make(chan int, 100)
-	go isUnique(intCh, arr)
-	for i := 0; i < 50; i++ {
+	amount := flag.Int("amount", 1, "")
+	flag.Parse()
+	defer wg.Done()
+	arr := make([]int, 0, *amount)
+	ch := make(chan int)
+	for i := 0; i < *amount; i++ {
 		wg.Add(1)
-		go generator(intCh)
-		//time.Sleep(time.Millisecond*5000)
+		go generator(ch, arr, *amount)
 	}
-	//sort.Ints(arr)
-	//fmt.Println(arr)
-	// close(intCh)
+	wg.Add(1)
+	go isUnique(ch, arr, *amount)
 	wg.Wait()
-	//close(intCh)
-	//close(intCh)
 	os.Exit(0)
 }
-
-// func main() {
-// 	arr := make([]int, 0)
-// 	var x int
-// 		var i int
-// 		//var rSrc Source
-// 	for len(arr) < 100 {
-// 		var rSrc cryptoSource
-//     rnd := rand.New(rSrc)
-
-//     // действительно случайное число от 0 до 999
-//     x=rnd.Intn(1000)
-// 		//x = rand.New(rSrc)
-// 		for i := 0; i < len(arr) && x == arr[i]; i++ {
-// 			fmt.Print(fmt.Sprintf("%d ",x))
-// 		}
-// 		if i == len(arr) {
-// 			arr = append(arr, x)
-// 		} else {
-// 			continue
-// 		}
-// 	}
-// 	fmt.Println(arr)
-// 	src := flag.String("src", "/", "") //флаги
-// 	to := flag.String("to", "/", "")
-// 	flag.Parse()
-// 	fmt.Println(*src, *to)
-// 	file, err := os.Open(*src)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer file.Close()
-
-// 	scanner := bufio.NewScanner(file)
-// 	for scanner.Scan() {
-// 		resp, err := http.Get("https://" + scanner.Text()) //GET
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			continue
-// 		}
-// 		defer resp.Body.Close()
-// 		body, err := ioutil.ReadAll(resp.Body) //подготовка к записи тела в файл
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 		wg.Add(1)                                  //увеличение счётчика wg
-// 		go responseWrite(to, scanner.Text(), body) //запись в файл тело ответа на GET
-// 		if err := scanner.Err(); err != nil {
-// 			fmt.Println(err)
-// 		}
-// 	}
-// 	wg.Wait()
-// }
